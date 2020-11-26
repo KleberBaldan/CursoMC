692d9425eee76dcb0fe8d85a2b7777b710bfa8c5
KleberBaldan <kleberbaldan@gmail.com>
2020-11-26 10:27:53 -0300

    Alterado Banco de Dados para uso do MySQL via Docker e criado camadas de repositório e serviço para Categoria.

---
+++ src/main/java/com/kleberbaldan/estudo/cursomc/repositories/CategoriaRepository.java
@@ -0,0 +1,9 @@
+package com.kleberbaldan.estudo.cursomc.repositories;
+
+import com.kleberbaldan.estudo.cursomc.domain.Categoria;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
+}
